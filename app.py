ed88f14f4ab61d75ea20b626eb233a6af8a9caa8
PamelaEsp <pamela.emc@comunidad.unam.mx>
2023-12-05 04:20:40 -0600

    Serv web, se cambio el form

--- app.py
+++ app.py
@@ -16,15 +16,11 @@ def index():
 @app.route('/control', methods=['POST'])
 def control():
     # Obtener datos del formulario
-    fan_power = int(request.form['fan_power'])
-    light_power = int(request.form['light_power'])
-    min_temp = int(request.form['min_temp']) #Si tem < min se apaga ventilador 
-    max_temp = int(request.form['max_temp']) #tem > max se enciende ventilador 
-    temp_cycle = int(request.form['temp_cycle'])
-    irrigate_cycle = int(request.form['irrigate_cycle'])
-
+    accion = int(request.form['accion'])
+    value_ac = float(request.form['value_ac'])
+    
     # Enviar datos al Arduino a través de I2C
-    data = [fan_power, light_power, min_temp, max_temp, temp_cycle, irrigate_cycle]
+    data = [accion, value_ac]
     #bus.write_i2c_block_data(ARDUINO_ADDRESS, 0, data)
     print(data)
     return render_template('index.html', message='Datos enviados con éxito.')
